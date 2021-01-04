48813e8f6727a3c2503a9962345c02d79f5353a5
xxzl0130 <zaxs0130@gmail.com>
2021-01-04 17:18:58 +0800

    add sharp

---
+++ SharpTest/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace SharpTest
+{
+    class Program
+    {
+        public static void Publisher()
+        {
+            using (var publisher = new PublisherSocket())
+            {
+                publisher.Bind("tcp://*:5556");
+
+                int i = 0;
+
+                while (true)
+                {
+                    publisher
+                        .SendMoreFrame("A") // Topic
+                        .SendFrame(StructureToByte(i), Marshal.SizeOf(i)); // Message
+
+                    i++;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public static void Subscriber()
+        {
+            using (var subscriber = new SubscriberSocket())
+            {
+                subscriber.Connect("tcp://127.0.0.1:5556");
+                subscriber.Subscribe("A");
+
+                while (true)
+                {
+                    var topic = subscriber.ReceiveFrameString();
+                    var msg = subscriber.ReceiveFrameBytes();
+                    var i = ByteToStructure<int>(msg);
+                    Console.WriteLine("From Publisher: {0} {1}", topic, i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var subThread = new Thread(Subscriber);
+            var pubThread = new Thread(Publisher);
+            subThread.IsBackground = true;
+            pubThread.IsBackground = true;
+            subThread.Start();
+            pubThread.Start();
+
+            Console.ReadKey();
+        }
+
+        public static byte[] StructureToByte<T>(T structure)
+        {
+            int size = Marshal.SizeOf(typeof(T));
+            byte[] buffer = new byte[size];
+            IntPtr bufferIntPtr = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.StructureToPtr(structure, bufferIntPtr, true);
+                Marshal.Copy(bufferIntPtr, buffer, 0, size);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(bufferIntPtr);
+            }
+            return buffer;
+        }
+
+        public static T ByteToStructure<T>(byte[] dataBuffer)
+        {
+            object obj = null;
+            int size = Marshal.SizeOf(typeof(T));
+            IntPtr allocIntPtr = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.Copy(dataBuffer, 0, allocIntPtr, size);
+                obj = Marshal.PtrToStructure(allocIntPtr, typeof(T));
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(allocIntPtr);
+            }
+            return (T)obj;
+        }
+    }
+}
