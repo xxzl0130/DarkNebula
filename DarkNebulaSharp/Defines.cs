bd7140eb7bc856033dee96ae06378fda69c58198
xxzl0130 <zaxs0130@gmail.com>
2021-01-04 20:45:21 +0800

    添加DarkNebulaSharp

---
+++ DarkNebulaSharp/Defines.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DarkNebulaSharp
+{
+    public enum SimStates
+    {
+		SimNop = 0,
+        SimInit,        // 初始化
+        SimStop,        // 停止中
+        SimRun = 0x10,  // 运行中
+        SimPause,       // 暂停中
+        SimStep,        // 单步模式
+	}
+
+    public enum ReplayStates
+    {
+        ReplayNop = 0,
+        Recording = 0x10,       // 记录数据
+        Replaying = 0x20,		// 重放数据
+    }
+
+    // 指令
+    public enum CommandCode
+    {
+        COMMAND_NOP = 0,
+        COMMAND_INIT,               // 初始化
+        COMMAND_REG,                // 注册
+        COMMAND_START,              // 开始
+        COMMAND_STEP_FORWARD,       // 向前推进一步
+        COMMAND_STEP_BACKWARD,      // 向后推进一步
+        COMMAND_STEP_IN,            // 单步模式
+        COMMAND_PAUSE,              // 暂停
+        COMMAND_STOP,               // 结束
+    };
+
+    public struct CommandHeader
+    {
+        // 节点ID，-1为全部
+        public Int32 ID;
+        public Int32 code;
+        public UInt32 size;
+    }
+
+    public static class DNVars
+    {
+        public const int ALL_NODE = -1;
+        public const string COMMAND_TOPIC = "command";
+        public const int COMMAND_TOPIC_LEN = 7;
+        public const string REPLY_TOPIC = "reply";
+        public const int REPLY_TOPIC_LEN = 5;
+        public const UInt16 ADMIN_RECEIVE_PORT = 6666;
+        public const UInt16 ADMIN_SEND_PORT = 8888;
+        public const UInt16 CHUNK_PORT = 10000;
+        public const string RECORD_FILE_SUFFIX = ".dnr";
+        public const UInt32 RECORD_FILE_MAGIC = 0x44417A9F;
+    }
+}
---
+++ DarkNebulaSharp/Node.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+using NetMQ.Core;
+
+namespace DarkNebulaSharp
+{
+    abstract class Node
+    {
+        protected Node()
+        {
+            PubSocket = null;
+            SubSocket = null;
+            WorkThread = null;
+            WorkStop = false;
+            SimSteps = 0;
+            CurTime = 0;
+            SimTime = 10;
+            SimFree = false;
+            StepTime = 10;
+            SimState = SimStates.SimNop;
+            ReplayState = (int) ReplayStates.ReplayNop;
+            RecordName = "";
+            WorkMutex = new Mutex(false);
+        }
+
+        ~Node()
+        {
+        }
+
+        // 发布指令的socket
+        protected PublisherSocket PubSocket;
+        // 接收回报的socket
+        protected SubscriberSocket SubSocket;
+        // 工作线程
+        protected Thread WorkThread;
+        // 监听线程停止标志
+        protected bool WorkStop;
+        // 监听锁
+        protected Mutex WorkMutex;
+        
+        // 仿真步数
+        public UInt32 SimSteps { get; }
+
+        // 当前仿真时间
+        public double CurTime { get; }
+        // 最长仿真时间
+        public double SimTime { get; set; }
+        // 自由仿真类型
+        public bool SimFree { get; set; }
+        // 仿真步长,ms
+        public UInt32 StepTime { get; set; }
+        // 仿真状态
+        public SimStates SimState { get; }
+        // 回放状态
+        protected int ReplayState;
+        // 记录名称
+        public string RecordName { get; set; }
+
+        // 开始工作线程
+        protected void StartWorking()
+        {
+            if (WorkThread != null)
+            {
+                StopWorking();
+                WorkThread = null;
+            }
+
+            WorkThread = new Thread((() =>
+            {
+                WorkMutex.WaitOne();
+                while (!WorkStop)
+                {
+                    Working();
+                }
+                WorkMutex.ReleaseMutex();
+            })) {IsBackground = true};
+            WorkThread.Start();
+        }
+        // 停止工作线程
+        protected void StopWorking()
+        {
+            WorkStop = true;
+            WorkMutex.WaitOne();
+            WorkStop = false;
+            WorkThread = null;
+        }
+        // 工作线程
+        protected abstract void Working();
+    }
+}
